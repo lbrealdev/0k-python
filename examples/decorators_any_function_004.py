8dbb89f98e43c8340130cc3399e4cd41bd8d0666
lbrealdev <lbrealdeveloper@gmail.com>
2025-06-23 22:21:27 +0200

    fix: final thoughts

--- examples/decorators_any_function_004.py
+++ examples/decorators_any_function_004.py
@@ -1,6 +1,13 @@
 # Example from this blog:
 # https://www.thepythoncodingstack.com/p/demystifying-python-decorators
 
+# A decorator is a function that accepts another function as an argument and returns yet another function.
+# The function it returns is a decorated version of the function you pass as an argument.
+
+# There are three functions involved in this definition, so it can get a bit complex.
+# The decorator, such as store_arguments(), is a function. Its argument and its return value are also functions.
+# These two functions perform similar tasks to each other. However, the function the decorator returns is a
+# decorated version of the function you pass as an argument to the decorator.
 
 # Outer function
 def store_arguments(func):
