f305346045d7f8e95fa3c7153896d79e3ce5f54e
lbrealdev <lbrealdeveloper@gmail.com>
2025-08-04 11:06:17 +0200

    feat: add python list example

---
+++ examples/001_list.py
@@ -0,0 +1,79 @@
+"""Python: working with list"""
+
+result = [
+    {
+        "Coin": "Akash",
+        "Price": "2.10",
+        "Max. supply": "20931",
+        "Circulating supply": "103405"
+    },
+    {
+        "Coin": "Bitcoin",
+        "Price": "113096.71",
+        "Max. supply": "21000000",
+        "Circulating supply": "19901015"
+    }
+]
+
+# New empty lists
+key_fields = []
+value_fields = []
+
+# Add value to list '1'
+key_fields.append(1)
+value_fields.append(9)
+
+# Add values to the list:
+if result:
+    keys = list(result[0].keys())
+    values = list(result[0].values())
+
+    for key in keys:
+        key_fields.append(key)
+
+    for value in values:
+        value_fields.append(value)
+
+# Debug original lists:
+print(f"Original list (keys): {key_fields}")
+print(f"Original list (values): {value_fields}", end="\n\n")
+
+# Remove item from lists by index.
+key_fields.pop(0)
+value_fields.pop(0)
+
+# Debug modified lists:
+print(f"Modified list (keys): {key_fields}")
+print(f"Modified list (values): {value_fields}", end="\n\n")
+
+# Remove all items from list.
+key_fields.clear()
+value_fields.clear()
+
+# Debug empty list:
+print(f"Empty list (keys): {key_fields}")
+print(f"Empty list (values): {value_fields}", end="\n\n")
+
+# Add new value to empty lists.
+key_fields.append("one")
+value_fields.append("five")
+
+# Add new value by index.
+key_fields.insert(0, "two")
+value_fields.insert(0, "six")
+
+# Reverse list.
+key_fields.reverse()
+value_fields.reverse()
+
+# Debug lists:
+print(f"Reverse list (keys): {key_fields}")
+print(f"Reverse list (values): {value_fields}", end="\n\n")
+
+# Remove first item from lists.
+key_fields.remove("two")
+value_fields.remove("five")
+
+# Debug removed list:
+print(f"Removed first item (keys): {key_fields}")
+print(f"Removed first item (values): {value_fields}")
